7034258a889acf20b3378839ef48738bfff409a6
Mithlesh Rawte <119120802+mithlesh-bit@users.noreply.github.com>
2024-01-25 23:01:20 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.set('views', path.join("./views", 'views'));
 app.use(router);
 
 
